66617a287cb9d12e53bdab71f32293508ee60f10
manprit619 <manprit619@gmail.com>
2020-11-22 17:31:18 +0100

    tiktok server commit

---
+++ data.js
@@ -0,0 +1,31 @@
+const data = [
+
+    {
+url:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+  channel:"manprit007",
+  description:" Yo this works!",
+  song:"99 problems but REACT AINT ONE!",
+  likes:"123",
+  messages:"456",
+    shares:"789",
+
+
+    },
+
+
+    {
+
+        url:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+        channel:"vsingh007",
+        description: "this is insane!",
+        song:"I hate you like I love you",
+        likes:"234",
+        messages:"762",
+        shares:"789",
+
+    },
+
+
+];
+
+export default data;
